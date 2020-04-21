8b01820706e5ba3c243f0ec4c948beba6a07814e
skop01 <opkm2002@hanmail.net>
2020-04-21 12:43:11 +0900

    Updates

---
+++ precache-manifest.ba12e5a03c87bf54a1f13c9981b8cca7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450dfe65312284d56029508594436587",
+    "url": "/skop/index.html"
+  },
+  {
+    "revision": "d934a32559c03e3e2215",
+    "url": "/skop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9cff71708d742a2ae5d7",
+    "url": "/skop/static/js/2.eb5613ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skop/static/js/2.eb5613ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d934a32559c03e3e2215",
+    "url": "/skop/static/js/main.17102e57.chunk.js"
+  },
+  {
+    "revision": "92b13797b4eaf9721b3f",
+    "url": "/skop/static/js/runtime-main.271008e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/skop/static/media/logo.5d5d9eef.svg"
+  }
+]);
